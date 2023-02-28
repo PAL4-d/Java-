c370e6d04fff5527b78b0acd498f08afbbb576f7
PAL4-d <you@example.com>
2023-02-28 22:16:15 +0800

    First day

---
+++ Java learn/src/learn/zhushi.java
@@ -0,0 +1,13 @@
+package learn;
+
+public class zhushi {
+    public static void main(String[] args) throws Exception {
+        // System.out.println("Hello, World!");单行注释
+
+        /* System.out.println("Hello, World!");多行注释*/
+
+         /*System.out.println("Hello, World!");文档注释
+          * 
+          */
+    }
+}
