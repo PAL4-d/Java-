097d83591a2aed32dffe94b0929c4f07f0970b07
PAL4-d <you@example.com>
2023-03-10 05:19:53 +0800

    2023.3.10 PM

---
+++ Java learn/src/learn/shuzubianli.java
@@ -0,0 +1,11 @@
+package learn;
+
+public class shuzubianli {
+    public static void main(String[] args) {
+        int [] arr = {1,2,3,4,5};
+        // 使用for循环遍历数组的元素
+        for (int i =0;i < arr.length;i++){
+            System.out.println(arr[i]);
+        }
+    }
+}
---
+++ Java learn/src/learn/test.java
@@ -0,0 +1,14 @@
+package learn;
+
+public class test {
+    public static void main(String[] args) {
+        int a = 1;
+        int b = 2;
+        int c = a + b;
+        System.out.println(c);
+
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i);
+        }
+    }
+}
